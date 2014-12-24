297b4f19230b7c50d23e4e03f52c80ffee30f9b2
Karim H Kyler <karim.kyler@gmail.com>
2014-12-24 12:05:55 -0800

    the map works!!

--- stats.js
+++ stats.js
@@ -1,54 +1,53 @@
+//var Firebase = require("firebase");
 
-
-function initialize() {
-
-  
-  var map = new google.maps.Map(d3.select("#map").node(), {
+var mapOptions = {
     zoom: 8,
     center: new google.maps.LatLng(37.76487, -122.41948),
     mapTypeId: google.maps.MapTypeId.TERRAIN
-  });
-}
-
-
-d3.xhr(/*firebase url goes here*/"", function(data) {
-  var overlay = new google.maps.OverlayView();
-
-  overlay.onAdd = function() {
-      var layer = d3.select(this.getPanes().overlayLayer).append("div")
-          .attr("class", "crimes");
-  overlay.draw = function() {
-        var projection = this.getProjection(),
-            padding = 10;
-
-        var marker = layer.selectAll("svg")
-            .data(d3.entries(data))
-            .each(transform) // update existing markers
-          .enter().append("svg:svg")
-            .each(transform)
-            .attr("class", "marker");
-
-        // Add a circle.
-        marker.append("svg:circle")
-            .attr("r", 4.5)
-            .attr("cx", padding)
-            .attr("cy", padding);
-
-        // Add a label.
-        marker.append("svg:text")
-            .attr("x", padding + 7)
-            .attr("y", padding)
-            .attr("dy", ".31em")
-            .text(function(d) { return d.key; });
-
-        function transform(d) {
-          d = new google.maps.LatLng(d.value[1], d.value[0]);
-          d = projection.fromLatLngToDivPixel(d);
-          return d3.select(this)
-              .style("left", (d.x - padding) + "px")
-              .style("top", (d.y - padding) + "px");
-        }
-      };
   };
-});
-overlay.setMap(map);
+
+  
+  var map = new google.maps.Map(d3.select("#map").node(),mapOptions);
+//google.maps.event.addDomListener(window, 'load', map);
+
+// d3.xhr(/*firebase url goes here*/"", function(data) {
+//   var overlay = new google.maps.OverlayView();
+
+//   overlay.onAdd = function() {
+//       var layer = d3.select(this.getPanes().overlayLayer).append("div")
+//           .attr("class", "crimes");
+//   overlay.draw = function() {
+//         var projection = this.getProjection(),
+//             padding = 10;
+
+//         var marker = layer.selectAll("svg")
+//             .data(d3.entries(data))
+//             .each(transform) // update existing markers
+//           .enter().append("svg:svg")
+//             .each(transform)
+//             .attr("class", "marker");
+
+//         // Add a circle.
+//         marker.append("svg:circle")
+//             .attr("r", 4.5)
+//             .attr("cx", padding)
+//             .attr("cy", padding);
+
+//         // Add a label.
+//         marker.append("svg:text")
+//             .attr("x", padding + 7)
+//             .attr("y", padding)
+//             .attr("dy", ".31em")
+//             .text(function(d) { return d.key; });
+
+//         function transform(d) {
+//           d = new google.maps.LatLng(d.value[1], d.value[0]);
+//           d = projection.fromLatLngToDivPixel(d);
+//           return d3.select(this)
+//               .style("left", (d.x - padding) + "px")
+//               .style("top", (d.y - padding) + "px");
+//         }
+//       };
+//   };
+// overlay.setMap(map);
+// });
